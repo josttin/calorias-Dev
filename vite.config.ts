2bb67f394ad4d501b19238a1c46f8fc34ac9ee4d
josttin <josttin2003@gmail.com>
2026-05-24 21:53:53 -0500

    fix: correct base path casing in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/CaloriasDev/',
+    base: '/calorias-Dev/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
