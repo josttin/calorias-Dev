3b0ed70c9155312d2a738659546aba19125e8806
josttin <62473924+josttin@users.noreply.github.com>
2026-05-24 23:19:47 -0500

    Refactor Vite config to remove PWA settings
    Removed PWA plugin configuration and updated base path.

--- vite.config.ts
+++ vite.config.ts
@@ -1,42 +1,12 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import { defineConfig } from 'vite';
-import { VitePWA } from 'vite-plugin-pwa';
+import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/calorias-Dev/',
-    plugins: [
-      react(), 
-      tailwindcss(),
-      VitePWA({
-        registerType: 'autoUpdate',
-        includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'maskable-icon.svg'],
-                manifest: {
-          name: 'Contador de Calorías',
-          short_name: 'CaloríasDev',
-          description: 'Aplicación para el seguimiento de calorías con Google AI Studio',
-          theme_color: '#ffffff',
-          background_color: '#ffffff',
-          display: 'standalone',
-          start_url: '/calorias-Dev/',
-          icons: [
-            {
-              src: 'https://cdn-icons-png.flaticon.com/192/10433/10433048.png',
-              sizes: '192x192',
-              type: 'image/png'
-            },
-            {
-              src: 'https://cdn-icons-png.flaticon.com/512/10433/10433048.png',
-              sizes: '512x512',
-              type: 'image/png'
-            }
-          ]
-        }
-
-      })
-    ],
+    base: './',
+    plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
